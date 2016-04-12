75f599c3ab64c6110c514bc8f879d51e64526d74
fightingit <fightingit@163.com>
2016-04-12 19:20:33 +0800

    添加mybatis,并能跑通

--- app-service/src/main/java/com/qiku/service/impl/UserServiceImpl.java
+++ app-service/src/main/java/com/qiku/service/impl/UserServiceImpl.java
@@ -1,16 +1,23 @@
 package com.qiku.service.impl;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
+import com.qiku.dao.UserMapper;
+import com.qiku.model.User;
 import com.qiku.service.IUserService;
 
 @Service("userService")
 public class UserServiceImpl implements IUserService {
+	@Resource
+	private UserMapper userMapper;
 
 	@Override
 	public String getUser(String name) {
 		// TODO Auto-generated method stub
-		return "大家好，我叫" + name;
+		User user = userMapper.selectByPrimaryKey(2);
+		return "大家好，我叫" + user.getName();
 	}
 
 }
