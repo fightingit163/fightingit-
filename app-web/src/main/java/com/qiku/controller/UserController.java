b735baf93ed2d869df66a685d0034e2faac2e6f9
fightingit <fightingit@163.com>
2016-04-12 16:02:15 +0800

    增加log4j日志

--- app-web/src/main/java/com/qiku/controller/UserController.java
+++ app-web/src/main/java/com/qiku/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.qiku.controller;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +17,7 @@ import com.qiku.service.IUserService;
 
 @Controller
 public class UserController {
+	private static Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Resource
 	private IUserService userService;
@@ -21,6 +26,8 @@ public class UserController {
 	public String getUser(HttpServletRequest request, HttpServletResponse response){
 		System.out.println("dddddddddddddddddddd");
 		System.out.println(userService.getUser("张三  4月12号改动   "));
+		logger.info("调用user， 时间{}", new Date());
+		logger.debug("debug调用user， 时间{}", new Date());
 		return "index";
 	}
 }
